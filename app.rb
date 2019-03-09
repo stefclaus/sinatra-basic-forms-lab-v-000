f21908488a508b8c6f09e5076839dfaac551d472
Stef Claus <stef.claus@gmail.com>
2019-03-09 21:36:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
 
   post '/puppy' do
     puppy = Puppy.new
-    @name = puppy.name(params[:user_phrase])
+    @name = puppy.name(params[:name])
     erb :display_puppy
   end
 

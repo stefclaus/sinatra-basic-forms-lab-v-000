d512c2f49559798c939fbf53c6a8c4225ce69c88
Stef Claus <stef.claus@gmail.com>
2019-03-09 21:40:48 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,11 +1,11 @@
 class Puppy
   attr_accessor :name, :age
-  attr_reader :breed 
+  attr_reader :breed
 
     def initialize(name, breed, age)
       @name = name
       @breed = breed
       @age = age
-    end  
+    end
   
 end
